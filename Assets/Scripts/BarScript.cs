7440f1a3d42a60d01e7bb239d74af88179b39022
Jacob <jacobhlnhtxb16@gmail.com>
2018-09-28 14:41:00 +0200

    Fuelmechanic + DamageScript fjernet
    +Fuelbaren virker
    +Man får fuel når man ikke roterer
    +Man mister fuel når man roterer
    +Modstandere dør kun når man roterer
    +fjernet damage script, da det er reworked over i Rotator scriptet

---
+++ Assets/Scripts/BarScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class BarScript : MonoBehaviour {
+
+    public float fillAmount;
+
+    public Image content;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Handlebar();	
+	}
+
+    private void Handlebar()
+    {
+        content.fillAmount = Map(fillAmount,0,100,0,1);
+    }
+
+    private float Map(float value, float inMin, float inMax, float outMin, float outMax)
+    {
+        return (value - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+        //(78 - 0) * (1 - 0) / (230 - 0) + 0;
+        //   78   * 1 / 230 = 0,339
+    }
+}
--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -3,15 +3,47 @@ using System.Collections;
 
 public class Rotator : MonoBehaviour
 {
+    public float fuelUse;
+    public float fuelGain;
 
     public float speed;
+
+    public int damage;
+
+    public bool isRotating;
 
     //Update is called every frame
     void Update()
     {
-        if(Input.GetMouseButton(0))
+        //når man trykker på knappen
+        if (Input.GetMouseButton(0))
         {
-            transform.Rotate(new Vector3(0, 0, speed) * Time.deltaTime);
+            //finder FuelMeterets BarScript -> Hvis Der er mere fuel end 0
+            if (GameObject.Find("FuelMeter").GetComponent<BarScript>().fillAmount > 0)
+            {
+                //så roterer den
+                transform.Rotate(new Vector3(0, 0, speed) * Time.deltaTime);
+                //så bruger den fuel
+                GameObject.Find("FuelMeter").GetComponent<BarScript>().fillAmount -= fuelUse;
+                isRotating = true;
+            }
+            else
+                isRotating = false;
+        }
+        //ellers hvis man ikke har maximum fuel
+        else if (GameObject.Find("FuelMeter").GetComponent<BarScript>().fillAmount < 100)
+        {
+            //så får man mere fuel
+            GameObject.Find("FuelMeter").GetComponent<BarScript>().fillAmount += fuelGain;
+            isRotating = false;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Enemy" && isRotating)
+        {
+            other.GetComponent<Enemy>().health -= damage;
         }
     }
 }
